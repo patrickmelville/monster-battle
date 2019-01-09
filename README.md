faea95e6b6232fe9460e6bc881f5f07d77baf49d
Patrick M <pmelville321@gmail.com>
2019-01-08 18:57:39 -0600

    updated readme with todo list & used new monster classes in testing file.

--- README.md
+++ README.md
@@ -13,8 +13,23 @@ A fun way to learn about Class Inheritance and Polymorphism in JAVA
 - Build two interfaces for any being class: Fighter, MagicUser
 
 ### Phase 2: Test your Warrior Classes
-- Pass all 8 tests
-- Send your code to the teacher
+- Your objects need to pass all 8 test in the "test()" method provided to you.
+- Experiment with different warrior types and stat point distributions to create the best warrior you can.
+- Send your object code to the teacher. (this will just be the one line of code to create your object)
 
 ### Phase 3: Put your Warriors in Battle and Defeat your Classmates
 - Teacher will plugin the code for each student and watch the mayhem in the tournament class
+
+# todo list
+ - refine attacks/defenses for each class
+ - - 1 attack & 1 def for human/monster
+ - - 1 special ability for h/m subclasses (4 total)
+ - - 1 for each interface
+ - create the battle method
+ - include an image for the objects, must be a 200x200 png/jpg
+ - 
+ 
+
+
+(possiblely... still thinking about this...)
+### Phase 4: Expand on this idea
--- Testing.java
+++ Testing.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -9,16 +8,8 @@ import javafx.stage.Stage;
 
 public class Testing extends Application{
     public void start(Stage stage){
-        // create two new animals
-        Animal c = new Cat("fluffy");
-        Animal d = new Dog("spot");
-        // create an ArrayList of type Animal
-        ArrayList<Animal> aList = new ArrayList<>();
-        // Add any animal that is a child of Animal class
-        aList.add(c); // this is a Cat object
-        aList.add(d); // this is a Dog object
-        // print out my list of Animals
-        System.out.println(aList);
+        Troll h1 = new Troll("Kronk", 25,25,25,25);
+        Mage h2 = new Mage("Gandalf", 25,25,25,25);
 
         //Creating a group
         Group root = new Group();
@@ -27,7 +18,7 @@ public class Testing extends Application{
 
         //Create and setup a title Text object
         Text title = new Text();
-        title.setText("WECOME TO MONSTER BATTLE");
+        title.setText("Welcome to Codémon!");
         title.setX(scene.getWidth()/3);
         title.setY(50);
         title.setFont(Font.font("Arial", 20));
@@ -35,8 +26,19 @@ public class Testing extends Application{
         root.getChildren().add(title);
 
 
+        // create new text to print out being 1 info
+        Text player1 = new Text();
+        player1.setText(h1.toString());
+        player1.setX(50);
+        player1.setY(100);
+        root.getChildren().add(player1);
 
-
+        // create new text to print out being 2 info
+        Text player2 = new Text();
+        player2.setText(h2.toString());
+        player2.setX(350);
+        player2.setY(100);
+        root.getChildren().add(player2);
 
         //Adding a title to the window
         stage.setTitle("Monster Battle");
